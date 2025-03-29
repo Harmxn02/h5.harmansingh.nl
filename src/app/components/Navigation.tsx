e29d28eb888a0a80c6624bef25246d240d018027
Harman Singh <harmandualsingh@gmail.com>
2025-03-29 19:23:11 +0100

    Add Navigation component and integrate it into the home page

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navigation = () => {
+	return (
+		<div className="border border-red-500 bg-white">
+			<h1 className="from-brand-blue to-brand-teal cursor-pointer bg-gradient-to-br bg-clip-text text-4xl font-black text-transparent">
+				H5.
+			</h1>
+		</div>
+	);
+};
+
+export default Navigation;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Navigation from "./components/Navigation";
+
 export default function Home() {
 	return (
-		<div className="min-h-screen font-[family-name:var(--font-geist-sans)]"></div>
+		<div className="min-h-screen font-[family-name:var(--font-geist-sans)] p-8 pb-20 gap-16 sm:p-8">
+			<Navigation />
+		</div>
 	);
 }
