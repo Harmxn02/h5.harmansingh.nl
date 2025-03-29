ff6b5d220a7f98fc3ea4264528b4ba50b3af7964
Harman Singh <harmandualsingh@gmail.com>
2025-03-29 20:16:46 +0100

    Add background image and gradient overlay to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Navigation from "./components/Navigation";
+import AdamBackground from "@/assets/images/adam.jpg";
 
 export default function Home() {
 	return (
-		<div className="min-h-screen font-[family-name:var(--font-geist-sans)] p-8 pb-20 gap-16 sm:p-8">
-			<Navigation />
+		<div className="relative min-h-screen max-w-screen overflow-hidden gap-16 font-[family-name:var(--font-geist-sans)]">
+			{/* Blurred Background */}
+			<div
+				className="absolute inset-0 bg-cover bg-center blur-xs"
+				style={{
+					backgroundImage: `url(${AdamBackground.src})`,
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+				}}
+			></div>
+
+			{/* Gradient Overlay */}
+			<div className="absolute inset-0 bg-gradient-to-b from-black/50 to-black/100"></div>
+
+			{/* Content */}
+			<div className="relative z-10">
+				<Navigation />
+			</div>
 		</div>
 	);
 }
