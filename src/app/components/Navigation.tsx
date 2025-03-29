f1a691d1bf718330b6119e89411f94f44f73417e
Harman Singh <harmandualsingh@gmail.com>
2025-03-29 20:17:18 +0100

    Add links to navigation bar

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -2,10 +2,51 @@ import React from "react";
 
 const Navigation = () => {
 	return (
-		<div className="border border-red-500 bg-white">
-			<h1 className="from-brand-blue to-brand-teal cursor-pointer bg-gradient-to-br bg-clip-text text-4xl font-black text-transparent">
-				H5.
-			</h1>
+		<div className="fixed top-0 z-50 w-full px-3 py-1 text-white transition sm:px-5">
+			<div className="mx-auto my-1.5 flex flex-row justify-between gap-2">
+				<div className="flex flex-row self-center text-xl font-medium">
+					<a href="/" className="md:self-center tracking-tight">
+						H5 Research
+					</a>
+				</div>
+				<div className="flex space-x-1 self-center">
+					<div className="group hidden w-fit shrink-0 items-center sm:flex">
+						<a
+							className="flex text-xs font-medium text-gray-300 group-hover:text-white transition"
+							href="/assets/files/whitepaper.pdf"
+							target="_blank"
+						>
+							External link
+							<div className="ml-1 text-gray-400 group-hover:text-white">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 16 16"
+									fill="currentColor"
+									className="h-4 w-4"
+								>
+									<path
+										fillRule="evenodd"
+										d="M4.22 11.78a.75.75 0 0 1 0-1.06L9.44 5.5H5.75a.75.75 0 0 1 0-1.5h5.5a.75.75 0 0 1 .75.75v5.5a.75.75 0 0 1-1.5 0V6.56l-5.22 5.22a.75.75 0 0 1-1.06 0Z"
+										clipRule="evenodd"
+									></path>
+								</svg>
+							</div>
+						</a>
+					</div>
+					<a
+						href="/"
+						className="flex self-center rounded-full bg-transparent px-2 py-0.5 text-xs font-medium text-gray-300 transition hover:text-white"
+					>
+						Link
+					</a>
+					<a
+						href="/"
+						className="self-center rounded-full bg-gray-100/10 px-2.5 py-1 text-xs font-medium text-gray-300 transition hover:bg-gray-100/20 hover:text-white"
+					>
+						Another
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 };
