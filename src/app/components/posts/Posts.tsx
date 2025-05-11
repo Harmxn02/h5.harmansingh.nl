c7f62f546e9af8a3607ab924a031add5d96881c1
Harman Singh <harmandualsingh@gmail.com>
2025-05-11 18:26:43 +0200

    Make largest grid 3 columns instead of 4

--- src/app/components/posts/Posts.tsx
+++ src/app/components/posts/Posts.tsx
@@ -9,7 +9,7 @@ const Posts = () => {
 			{/* <h2 className="text-3xl">Posts</h2> */}
 
 			{/* Map through the posts and render each one */}
-			<div className="grid grid-cols-1 justify-items-center gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+			<div className="grid grid-cols-1 justify-items-center gap-5 sm:grid-cols-2 xl:grid-cols-3">
 				{posts.map((post) => (
 					<Post
 						key={post.id}
